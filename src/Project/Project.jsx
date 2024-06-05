abe87600647f4ec2921d359fe7f475c2a191dde2
Sinth <sinthmite@gmail.com>
2024-06-04 23:22:23 -0400

    Video for Storm added

--- src/Project/Project.jsx
+++ src/Project/Project.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import './Project.css';
 import StormAtlas from '../assets/StormAtlas.png';
-import FlavorQuest from '../assets/FlavorQuest.png';
 import NourishPaw from '../assets/Nourish.mp4';
 import Storm from '../assets/Storm.mp4'
+import FlavorQuest from '../assets/tempFQ.mp4'
 export default function Project() {
     return (
         <div className="project-container">
@@ -59,8 +59,11 @@ export default function Project() {
 
             <div className="project-item">
                 <h3 className="project-name">FlavorQuest</h3>
-                <img src={FlavorQuest} alt="FlavorQuest" className="project-image" />
-                <div className="project-textHolding">
+                <video src={FlavorQuest} className="project-image" autoPlay muted loop playsInline></video>
+                <div className="project-links">
+                    <a href="https://flavorquests.netlify.app/" target="_blank" rel="noopener noreferrer">Live Project</a> | 
+                    <a href="https://github.com/SinthMite/FlavorQuest" target="_blank" rel="noopener noreferrer">GitHub Repository</a>
+                </div>                 <div className="project-textHolding">
                     <article className="project-description">
                         <h3>Summary</h3>
                         <p>"FlavorQuest" is a culinary exploration app designed to help users discover new recipes and cooking techniques. The application features robust search functionality, personalized meal planning, and seamless user authentication.</p>
